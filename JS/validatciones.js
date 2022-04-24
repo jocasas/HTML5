51726b691933d5a5853e64032d24ed1ad0430dbf
jocasas <jo.casas@duocuc.cl>
2022-04-24 00:18:44 -0400

    fix urls &&  vals(mismo event)

--- JS/validatciones.js
+++ JS/validatciones.js
@@ -50,12 +50,9 @@ $(document).ready(function () {
       });
     }
   });
-});
 
+  /* Validaciones Login */
 
-/* ----------------------      HACER card-img-top    ------------------------------------ */
-
-$(document).ready(function () {
   $("#btn-login").click(function (event) {
     var usuario = $("#usuario-login").val();
     if (usuario == "") {
@@ -68,9 +65,7 @@ $(document).ready(function () {
     }
 
   });
-});
 
-$(document).ready(function () {
   $("#btn-login").click(function (event) {
     var contrasenna = $("#contrasenna-login").val();
     if (contrasenna == "") {
@@ -83,76 +78,68 @@ $(document).ready(function () {
     }
 
   });
-});
-//VALIDACIONES REGISTRO
-//4.val rep clave
-$(bt_reg).click(function (event) {
-  var pass = $("#clave").val();
-  var pass2 = $("#clave2").val();
 
-  if (pass2 !== pass || pass2 == "") {
+  $("#bt_reg").click(function (event) {
 
-    $("#clave2").focus();
-    $("#error4").fadeIn();
-    $("#clave2").css({ 'borderColor': '#fa1b1b' })
-    event.preventDefault();
-  } else {
-    $("#error4").hide();
-    $("#clave").css({ 'borderColor': '#ffffff' })
-
-  }
-});
-
-//3.val clave
-$(bt_reg).click(function (event) {
-  var pass = $("#clave").val();
-  if (pass.length = "") {
-    $("#clave").focus();
-    $("#error3").fadeIn();
-    $("#clave").css({ 'borderColor': '#fa1b1b' })
-    event.preventDefault();
-  } else {
-    if (pass.length > 3 && pass.length < 9) {
-      $("#error3").hide();
-      $("#clave").css({ 'borderColor': '#ffffff' })
+    //variables
+    var pass = $("#clave").val();
+    var pass2 = $("#clave2").val();
+    var emailreg = /^([a-z A-Z 0-9_\.\-])+\@(([a-z A-Z 0-9\-])+\.)+([a-z A-Z 0-9]{2,4})+$/;
+    var email = $('#correo').val();
+    var usuario = $('#usuario').val();
+    
+    //4.val clave rep
+    if (pass2 !== pass || pass2 == "") {
+      $("#clave2").focus();
+      $("#error4").fadeIn();
+      $("#clave2").css({ 'borderColor': '#fa1b1b' })
+      event.preventDefault();
     } else {
+      $("#error4").hide();
+      $("#clave").css({ 'borderColor': '#ffffff' })
+    }
+
+  //3.val clave
+    if (pass.length = "") {
       $("#clave").focus();
       $("#error3").fadeIn();
       $("#clave").css({ 'borderColor': '#fa1b1b' })
-
       event.preventDefault();
-
+    } else {
+      if (pass.length > 3 && pass.length < 9) {
+        $("#error3").hide();
+        $("#clave").css({ 'borderColor': '#ffffff' })
+      } else {
+        $("#clave").focus();
+        $("#error3").fadeIn();
+        $("#clave").css({ 'borderColor': '#fa1b1b' })
+  
+        event.preventDefault();
+  
+      }
     }
-  }
-});
 
-//2.val correo
-$(bt_reg).click(function (event) {
-  var emailreg = /^([a-z A-Z 0-9_\.\-])+\@(([a-z A-Z 0-9\-])+\.)+([a-z A-Z 0-9]{2,4})+$/;
-  var email = $('#correo').val();
-
-  if (email == "" || !emailreg.test(email)) {
-    $("#correo").focus();
-    $("#error2").fadeIn();
-    $("#correo").css({ 'borderColor': '#fa1b1b' })
-    event.preventDefault();
-  } else {
-    $("#correo").css({ 'borderColor': '#ffffff' })
-    $("#error2").hide();
-  }
+  //2.val correo  
+    if (email == "" || !emailreg.test(email)) {
+      $("#correo").focus();
+      $("#error2").fadeIn();
+      $("#correo").css({ 'borderColor': '#fa1b1b' })
+      event.preventDefault();
+    } else {
+      $("#correo").css({ 'borderColor': '#ffffff' })
+      $("#error2").hide();
+    }
+  
+  //1.val usuario
+    if (usuario.length > 3 && usuario.length < 9) {
+      $("#error1").hide();
+      $("#usuario").css({ 'borderColor': '#ffffff' })
+    } else {
+      $("#usuario").focus();
+      $("#error1").fadeIn();
+      $("#usuario").css({ 'borderColor': '#fa1b1b' })
+      event.preventDefault();
+    }
+  });
+  
 });
-
-//1.val usuario
-$(bt_reg).click(function (event) {
-  var usuario = $('#usuario').val();
-  if (usuario.length > 3 && usuario.length < 9) {
-    $("#error1").hide();
-    $("#usuario").css({ 'borderColor': '#ffffff' })
-  } else {
-    $("#usuario").focus();
-    $("#error1").fadeIn();
-    $("#usuario").css({ 'borderColor': '#fa1b1b' })
-    event.preventDefault();
-  }
-});
-//VALIDACIONES REGISTRO
